d623fa73b8b7bca6e29f38d2f2d22b892aceb87b
jyc5331 <jcosgrovecoding@gmail.com>
2020-11-15 10:40:11 -0500

    startings of save button for local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,27 @@
-document.getElementById("currentDay").innerHTML = formatAMPM();
       // function for displaying Date at top of calendar
+
+document.getElementById("currentDay").innerHTML = formatAMPM();
+
       function formatAMPM() {
         var d = new Date(),
           months = ['January','February','March','April','May','June','July','August','September','October','November','December'],
           days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
         return days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate();
       }
-      // function for changing color in rows based on time, time works!
+
+      // variables for changing color in rows based on time
+      var row = Array.from(document.querySelectorAll(".row"))
       var today = new Date();
       var time = today.getHours()
       console.log(time);
+      console.log(row);
 
+      //local storage code
+      $('.saveBtn').on('click', function(event){
+    
+      });
+
+     // moment().hour(Number); was suggested by Chris to get time background shift up and running 
 
           /* HELP localstorage, make it so that when the save icon is clicked, contents of userInput are stored to local server
       window.onbeforeunload = function() {
